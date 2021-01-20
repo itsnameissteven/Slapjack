9894256690e20fdbba653b8c3e318eaba76a2186
Steven Mancine <stevenrmancine@gmail.com>
2021-01-19 17:15:26 -0700

    Format and refactor all files

--- card.js
+++ card.js
@@ -1,20 +1,22 @@
 class Card {
-    constructor() {
-        this.suits = ["blue", "red", "green", "gold"];
-        this.values = ["ace", "02", "03", "04", "05", "06", "07", "08", "09", "10", "jack", "queen", "king"];
-    }
-    buildCompleteDeck() {
-        var suitsIndex = 0;
-        var valuesIndex = 0;
-        var deck = [];
-        for(var i = 0; i < 52; i++){
-            if (valuesIndex === 13) {
-                suitsIndex++;
-                valuesIndex = 0;
-            };
-            deck.push({color: this.suits[suitsIndex], value: this.values[valuesIndex]});
-            valuesIndex++;
-        }
-        return deck;
-    }
+  constructor() {
+    this.suits = ["blue", "red", "green", "gold"];
+    this.values = ["ace", "02", "03", "04", "05", "06", "07", "08", "09", "10", "jack", "queen", "king"];
+  }; 
+
+  buildCompleteDeck() {
+    var suitsIndex = 0;
+    var valuesIndex = 0;
+    var deck = [];
+    for(var i = 0; i < 52; i++) {
+      if (valuesIndex === 13) {
+        suitsIndex++;
+        valuesIndex = 0;
+      };
+      deck.push({color: this.suits[suitsIndex], value: this.values[valuesIndex]});
+      valuesIndex++;
+    };
+
+    return deck;
+  };
 }
--- player.js
+++ player.js
@@ -1,15 +1,17 @@
 class Player {
-    constructor(player, playsFirst) {
-        this.id = player
-        this.wins = localStorage.getItem(this.id) || 0
-        this.hand = []
-        this.hasNextTurn = playsFirst || false
-        this.wonGame;
-    }
-    playCard() {
-        return this.hand.shift()
-    }
-    saveWinsToStorage() {
-        localStorage.setItem(this.id, this.wins)
-    }
+  constructor(player, playsFirst) {
+    this.id = player;
+    this.wins = localStorage.getItem(this.id) || 0;
+    this.hand = [];
+    this.hasNextTurn = playsFirst || false;
+    this.wonGame;
+  };
+
+  playCard() {
+    return this.hand.shift();
+  };
+
+  saveWinsToStorage() {
+    localStorage.setItem(this.id, this.wins);
+  };
 }
